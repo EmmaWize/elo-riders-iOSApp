1d61017b450bc5f5fa5ca831b3ac54ac6e230fbe
Emma <emmauel1997@mail.com>
2025-02-12 15:27:50 -0500

    Initial Commit

---
+++ harley/harleyApp.swift
@@ -0,0 +1,17 @@
+//
+//  harleyApp.swift
+//  harley
+//
+//  Created by Emmanuel Zambrano on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct harleyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
